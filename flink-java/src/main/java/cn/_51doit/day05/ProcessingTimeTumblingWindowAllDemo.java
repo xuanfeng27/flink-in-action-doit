d607880ab60dfcaf0b8f1b4825d582f1330450ed
start <zx@51doit.cn>
2021-10-20 11:58:55 +0800

    滚动窗口

---
+++ flink-java/src/main/java/cn/_51doit/day05/ProcessingTimeTumblingWindowAllDemo.java
@@ -0,0 +1,45 @@
+package cn._51doit.day05;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
+import org.apache.flink.streaming.api.windowing.triggers.PurgingTrigger;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+/**
+ * 不keyBy，直接划分滚动窗口，即按照ProcessingTime划分滚动窗口
+ * 底层调用的是windowAll方法，传入的是ProcessingTime类型的窗口
+ *
+ * 调用的是windowAll方法得到是的nonKeyedWindow，window已经window function算子对应的DataStream并行度为1
+ */
+public class ProcessingTimeTumblingWindowAllDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+        configuration.setInteger("rest.port", 8081);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        DataStreamSource<String> lines = env.socketTextStream("localhost", 8888);
+
+        SingleOutputStreamOperator<Integer> nums = lines.map(Integer::parseInt);
+
+        //滚动窗口，10秒滚动一次
+        //AllWindowedStream<Integer, TimeWindow> windowedStream = nums.timeWindowAll(Time.seconds(10));
+        AllWindowedStream<Integer, TimeWindow> windowedStream = nums.windowAll(TumblingProcessingTimeWindows.of(Time.seconds(10)));
+
+        SingleOutputStreamOperator<Integer> res = windowedStream.sum(0);
+
+        res.print();
+
+        env.execute();
+
+    }
+}
---
+++ flink-java/src/main/java/cn/_51doit/day05/ProcessingTimeTumblingWindowDemo.java
@@ -0,0 +1,58 @@
+package cn._51doit.day05;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
+import org.apache.flink.streaming.api.windowing.triggers.PurgingTrigger;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+/**
+ * 先keyBy，再按照ProcessingTime划分滚动窗口
+ *
+ *
+ */
+public class ProcessingTimeTumblingWindowDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+        configuration.setInteger("rest.port", 8081);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        //spark,3
+        DataStreamSource<String> lines = env.socketTextStream("localhost", 8888);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordAndCount = lines.map(line -> {
+            String[] fields = line.split(",");
+            String word = fields[0];
+            int count = Integer.parseInt(fields[1]);
+            return Tuple2.of(word, count);
+        }).returns(Types.TUPLE(Types.STRING, Types.INT));
+
+        //先调研keyBy
+        KeyedStream<Tuple2<String, Integer>, String> keyedStream = wordAndCount.keyBy(t -> t.f0);
+
+        //按照ProcessingTime划分滚动窗口
+        //WindowedStream<Tuple2<String, Integer>, String, TimeWindow> windowedStream = keyedStream.timeWindow(Time.seconds(10));
+
+        WindowedStream<Tuple2<String, Integer>, String, TimeWindow> windowedStream = keyedStream.window(TumblingProcessingTimeWindows.of(Time.seconds(10)));
+
+        //调用window function
+        SingleOutputStreamOperator<Tuple2<String, Integer>> res = windowedStream.sum(1);
+
+        res.print();
+
+        env.execute();
+
+    }
+}
